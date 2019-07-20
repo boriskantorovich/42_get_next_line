e73668b871ab8e52d175a6a820fe3462c4f87204
Draugr scourge Fisher <dfisher@at-r1.21-school.ru>
2019-07-20 19:59:54 +0300

    Prototyping gnl

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfisher <dfisher@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/20 19:13:47 by dfisher           #+#    #+#             */
+/*   Updated: 2019/07/20 19:39:17 by dfisher          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+# define	BUFF_SIZE 32
+# include <fcntl.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
