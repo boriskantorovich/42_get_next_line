5e9830dae361505a46253adcd29a4aae5870b665
Draugr scourge Fisher <dfisher@at-r4.21-school.ru>
2019-07-25 22:41:03 +0300

    trying to make gnl variadic

--- get_next_line.h
+++ get_next_line.h
@@ -6,60 +6,28 @@
 /*   By: dfisher <dfisher@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 19:13:47 by dfisher           #+#    #+#             */
-/*   Updated: 2019/07/25 21:29:24 by dfisher          ###   ########.fr       */
+/*   Updated: 2019/07/25 22:35:40 by dfisher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_GET_NEXT_LINE_H
 # define FT_GET_NEXT_LINE_H
 # include <fcntl.h>
-# include "libft.h"
+# include "libft/libft.h"
+
+# define G_CHRS 0x01
+# define G_TRIM 0x02
+# define G_LIST	0x04
+# define G_GREP	0x08
+# define G_EXCL	0x10
 
 /*
 ** Get optimal buff_size running
 ** $> diskutil info / | grep "Block Size"
 */
 
-# define BUFF_SIZE 9999
+# define BUFF_SIZE 4096
 # define ENDL '\n'
-
-/*
-** exclude this includes for release
-*/
-
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-
-typedef struct		s_file
-{
-	char			*content;
-	int				fildes;
-	int				flag:1;
-	struct s_file	*next;
-}					t_file;
-
-typedef struct	s_gnl
-{
-	char		*buf;
-	int			count;
-	int			i;
-	int			nl;
-	int			fd;
-}				t_gnl;
-
-typedef struct	s_f
-{
-	int				d;
-	int				s;
-	char			*b;
-	char			*t;
-	char			r[BUFF_SIZE];
-	int				i;
-	int				j;
-	int				k;
-	struct s_f		*n;
-}				t_f;
 
 /*
 ** Get max value of filedescriptor running
