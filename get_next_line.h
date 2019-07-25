55c773684b84ec6aab7c9e6eb48078f935999d2c
Draugr scourge Fisher <dfisher@at-r4.21-school.ru>
2019-07-25 23:51:17 +0300

    more variadic functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dfisher <dfisher@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 19:13:47 by dfisher           #+#    #+#             */
-/*   Updated: 2019/07/25 23:03:09 by dfisher          ###   ########.fr       */
+/*   Updated: 2019/07/25 23:27:11 by dfisher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,32 @@
 # define G_GREP	0x08
 # define G_EXCL	0x10
 
-typedef char* t_va_list;
 
-#define __va_argsiz(t)	\
-	(((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
-#define va_start(ap, pN)	\
-	((ap) = ((va_list) (&pN) + __va_argsiz(pN)))
-#define va_end(ap)	((void)0)
-#define va_arg(ap, t)					\
-	 (((ap) = (ap) + __va_argsiz(t)),		\
-	  *((t*) (void*) ((ap) - __va_argsiz(t))))
+/* STDARG.H*/
+/* Assume: width of stack == width of int. */
 
+#define	STACK_WIDTH	sizeof(int)
+
+/* Round up object width so it's an even multiple of STACK_WIDTH.
+Using AND for modulus here, so STACK_WIDTH must be a power of 2. */
+#define	TYPE_WIDTH(TYPE)				\
+	((sizeof(TYPE) + STACK_WIDTH - 1) & ~(STACK_WIDTH - 1))
+
+/* point the va_list pointer to LASTARG,
+then advance beyond it to the first variable arg */
+#define	ft_va_start(PTR, LASTARG)				\
+	PTR = (ft_va_list)((char *)&(LASTARG) + TYPE_WIDTH(LASTARG))
+
+/* Increment the va_list pointer, then "return"
+(evaluate to, actually) the previous value of the pointer. */
+#define ft_va_arg(PTR, TYPE)	(			\
+	PTR = (char *)(PTR) + TYPE_WIDTH(TYPE)		\
+				,			\
+	*((TYPE *)((char *)(PTR) - TYPE_WIDTH(TYPE)))	\
+				)
+#define ft_va_end(PTR)	/* nothing */
+
+typedef void *ft_va_list;
 /*
 ** Get optimal buff_size running
 ** $> diskutil info / | grep "Block Size"
--- main.c
+++ main.c
@@ -6,33 +6,141 @@
 /*   By: dfisher <dfisher@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 20:07:45 by dfisher           #+#    #+#             */
-/*   Updated: 2019/07/25 23:08:43 by dfisher          ###   ########.fr       */
+/*   Updated: 2019/07/25 23:49:59 by dfisher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+//#include "get_next_line.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
-/*
-** this is a test main function for gnl project. one should not submit
-** it to defense
-*/
-void		print_ints(int nums, ...)
+void simple_printf(const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    while (*fmt != '\0') {
+        if (*fmt == 'd') {
+            int i = va_arg(args, int);
+            printf("%d\n", i);
+        } else if (*fmt == 'c') {
+            // A 'char' variable will be promoted to 'int'
+            // A character literal in C is already 'int' by itself
+            int c = va_arg(args, int);
+            printf("%c\n", c);
+        } else if (*fmt == 'f') {
+            double d = va_arg(args, double);
+            printf("%f\n", d);
+        }
+        ++fmt;
+    }
+
+    va_end(args);
+}
+
+
+int combine_strings(char **outputStr,...)
+{
+    va_list ap;
+    char *s, *out=0;
+    int len=0;
+
+    va_start(ap,outputStr);
+    while((s=va_arg(ap,char *)))
+    {
+      len+=strlen(s);
+    }
+    va_end(ap);
+
+
+    if(!(out=malloc(len+1)))
+        exit(1);
+
+    *outputStr=out;
+
+    va_start(ap,outputStr);
+    while((s=va_arg(ap,char *)))
+    {
+      len=strlen(s);
+      memcpy(out,s,len);
+      out+=len;
+    }
+    va_end(ap);
+
+    *out=0;
+
+    return 0;
+}
+
+int		ft_add(int args, ...)
+{
+	va_list	ap;
+
+	va_start(ap, args);
+
+	int i = 0;
+	int sum = 0;
+
+	while (i < args)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+	return (sum);
+}
+
+void		ft_print_ints(int num, ...)
 {
 	va_list	args;
 
 	va_start(args, num);
 
-	for(int i = 0; i < num; i++)
+	int i = 0;
+	int value = 0;
+	while (i < num)
 	{
-
+		value = (int)va_arg(args,int);
+		printf("%d: %d\n", i, value);
+		i++;
 	}
+
+	va_end(args);
 }
-
-int			main(int argc, char **argv)
+/*
+** this is a test main function for gnl project. one should not submit
+** it to defense
+*/
+/*
+void		print_ints(int nums, ...)
 {
+	ft_va_list	args;
 
-	int		fd;
+	ft_va_start(args, nums);
+
+	for(int i = 0; i < nums; i++)
+	{
+		int value = ft_va_arg(args, int);
+		printf("%d: %d\n", i, value);
+	}
+
+	ft_va_end(args);
+}
+*/
+int			main(void)
+{
+	ft_print_ints(5, NULL);
+	simple_printf("dcff", 3, 'a', 1.999, 42.5);
+  char * res;
+  char * * output = &res;
+  combine_strings(output, "FOO", "BAR", "BAZ", NULL);
+  printf("%s\n", *output);
+	//ft_print_ints(10);
+	//int i = ft_add(1, 2, 3);
+	//printf("%d", i);
+/*	int		fd;
 	char	*line;
 
 	if (argc == 2)
@@ -42,7 +150,7 @@ int			main(int argc, char **argv)
 			ft_putstr(line);
 		ft_putchar('\n');
 		close(fd);
-	}
+	}*/
 	/*
 	char		*line;
 	int			fd;
